75f667019735278a1a39f8a91095ddfb93942ca7
Hieu <xuanhieu0012@gmail.com>
2021-08-11 16:37:39 -0500

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (array, name)=>{
+    const newArray =array.filter(name2 => name2.toUpperCase() === name.toUpperCase())
+    return newArray
+}
+
+// have to match first 2 letter
+
+const fuzzyMatch = (array, name) =>{
+    const newArray = array.filter(name2 => name2.substring(0,2) === name )
+    return newArray
+}
+
+
+const matchName = (array, name1) =>{
+    const newArray = array.filter(name2 => {
+        
+        if (name2.name === name1){
+            return name2
+        }
+        }
+    )
+    return newArray
+}
